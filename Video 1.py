03cb269e87853bfe4924de9f00a76c72f6547069
BryanSolares <solares.josue@outlook.com>
2019-12-24 19:38:55 -0600

    Inicio de Curso Python

---
+++ Video 1.py
@@ -0,0 +1,20 @@
+print("Hola Mundo")
+print("Adiós Mundo Cruel")
+
+#para comentarios utilizar simbolo -> #
+
+mi_nombre = "Mi nombre es Bryan Solares"
+
+#Se utiliza la barra invertida para agregar varías lineas para una sola instrucción
+mi_segundo_nombre = "Mi segundo nombres es \
+    Bryan Josue Solares"
+
+print(mi_nombre)
+print(mi_segundo_nombre)
+
+a = 0
+
+#implementación de identación y bucle
+for i in range(5):
+    print(a)
+    a+=1
